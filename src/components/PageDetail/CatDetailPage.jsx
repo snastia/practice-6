1abb974f41af1755d3fa42e5f3f298b15e0f77f3
Настя Скрицька <snastia.m@gmail.com>
2024-06-15 11:13:39 +0300

    classwork

--- src/components/PageDetail/CatDetailPage.jsx
+++ src/components/PageDetail/CatDetailPage.jsx
@@ -1,14 +1,17 @@
-import { Outlet, useNavigate, useParams } from "react-router-dom";
+import { Outlet, useLocation, useNavigate, useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 function CatDetailPage(){
     const {catId} = useParams()
     const navigate = useNavigate()
+    const location = useLocation()
+
+    console.log(location.state.from)
 
     return(
         <>
         <h1>Cat: {catId}</h1>
-        <Link to='/cats'>Go home</Link>
+        <Link to={`${location.state?.from ?? '/cats'}`}>Go home</Link>
         <button type="button" onClick={() => navigate('/', {replace: true})}>На головну</button>
         <ul>
             <li><Link to='description'>Description</Link></li>
--- src/components/Pages/CatsPage.jsx
+++ src/components/Pages/CatsPage.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { Link, useSearchParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 function CatsPage(){
   const [cats, setCats] = useState(['cat-1', 'cat-2', 'cat-3', 'cat-4'])
+  const location = useLocation()
   const [searchParams, setSearchParams] = useSearchParams('');
 
-  const catId = searchParams.get('name') || ''
+  const catId = searchParams.get('name') ?? ''
 
   const updateQueryString = (e) => {
     if(e.target.value !== ''){
@@ -25,8 +27,9 @@ function CatsPage(){
           <ul>
             {visibleCats().map(cat => {
             return (
-               <li>
-                <Link to={`${cat}`}>{cat}</Link>
+               <li key={cat}>
+                <Link to={`${cat}`} key={cat} cat={cat} 
+                state={{from: location}}>{cat}</Link>
                 </li>
             )
           })}
